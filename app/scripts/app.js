72f5372b5b5fe3c050937a60672d9af3c915cc2e
Praveen Yallakara <praveenyr@gmail.com>
2016-10-05 09:24:01 -0700

    Complete Assignment-5-Angular-Controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,9 +15,10 @@
               })
               .state('album', {
                   url: '/album',
+                  controller: 'AlbumCtrl as album',
                   templateUrl: '/templates/album.html'
                 })
-                .state('collection', {
+              .state('collection', {
                     url: '/collection',
                     controller: 'CollectionCtrl as collection',
                     templateUrl: '/templates/collection.html'

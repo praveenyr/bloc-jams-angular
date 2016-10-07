7ca883b08860e125c67dc1efd80fd9c952796926
Praveen Yallakara <praveenyr@gmail.com>
2016-10-06 21:01:45 -0700

    Assignment-9-Ang-Services-2

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -2,7 +2,7 @@
     function LandingCtrl() {
         this.heroTitle = "Turn the Music Up!";
     }
-console.log("Inside LandingCtrl");
+
     angular
         .module('blocJams')
         .controller('LandingCtrl', LandingCtrl);
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,7 +2,12 @@
     function SongPlayer() {
       var SongPlayer = {};
 
+      /**
+      * @desc Curently playing song
+      * @type {Object}
+      */
       var currentSong = null;
+
       /**
       * @desc Buzz object audio file
       * @type {Object}
@@ -28,24 +33,40 @@
          currentSong = song;
       };
 
+      /**
+      * @function playSong
+      * @desc Plays currently playing song and sets 'playing' property on song object to true
+      * @param {Object} song
+      */
+      var playSong = function(song) {
+          currentBuzzObject.play();
+          song.playing = true;
+          console.log(currentSong);
+          console.log(song);
+      };
+
       SongPlayer.play = function(song) {
          //If the clicked song is not the currently playing song
+         if (currentSong !== song) {
             setSong(song);
-            currentBuzzObject.play();
-            song.playing = true;
+            playSong(song);
             //Else if the clicked song is actually the currently playing song
           } else if (currentSong === song) {
                if (currentBuzzObject.isPaused()) {
-                   currentBuzzObject.play();
+                   playSong(song);
                }
           }
 
       };
 
       SongPlayer.pause = function(song) {
+        console.log("Inside Pause");
           currentBuzzObject.pause();
           song.playing = false;
+          console.log("currentBuzzObject is Paused ? : " + currentBuzzObject.isPaused());
+          console.log("song.playing :" + song.playing);
       };
+
      return SongPlayer;
    }
 
